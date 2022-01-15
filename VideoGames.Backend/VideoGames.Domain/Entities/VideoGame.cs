19134e29a55909233aebe87a025494977c4f13d6
Vladislav <50212442+vsnedro@users.noreply.github.com>
2022-01-15 17:26:39 +0300

    Added domain entities.

---
+++ VideoGames.Backend/VideoGames.Domain/Entities/VideoGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoGames.Domain.Entities;
+
+public class VideoGame
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateTime ReleaseDate { get; set; }
+
+    public DeveloperStudio DeveloperStudio { get; set; }
+
+    public IEnumerable<VideoGameGenre> Genres { get; set; } = new List<VideoGameGenre>();
+}
